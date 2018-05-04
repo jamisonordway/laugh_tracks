e12ebc42de6c46f6a586bdf33733cf7c0f32aa44
JamisonOrdway <jamisonordway@gmail.com>
2018-05-04 10:53:47 -0600

    WIP adds feature tests

---
+++ spec/features/user_can_see_number_of_specials_for_each_comedian.rb
@@ -0,0 +1,11 @@
+RSpec.describe 'User' do
+
+  it 'should show the number of specials associated with each comedian' do
+    comedian = Comedian.create(name: 'Bo Burnham', age: 23)
+    special_1 = Special.create(name: 'What', comedian_id: 1)
+    special_2 = Special.create(name: 'Idk', comedian_id: 1)
+    visit('/comedians')
+
+    expect(page).to have_content()
+  end
+end
